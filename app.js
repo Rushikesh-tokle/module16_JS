f47f5e01ccf7df37580ce684d2ac9c872b3c444e
rushi <rushikesh.tokle@gmail.com>
2025-05-02 09:02:57 +0530

    Template Literals and operators

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+let a=100;
+let b=20;
+console.log("the sum is ",a+b);
+let pencilPrice=10;
+let penPrice=5;
+
+let output =`the complete price is ${pencilPrice+penPrice} rupees` ;
+console.log(output);
+//Arithmetic Operators
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+console.log(a/b);
+
+
+console.log("Unary Operators");
+
+//Unary operators
+a++;
+console.log(a);
+a--;
+console.log(a);
+++a;
+console.log(a);
+--a;
+console.log(a);
+
