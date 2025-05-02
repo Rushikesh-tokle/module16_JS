4dc509418ef07d0468d45784fb101b470645b0b8
rushi <rushikesh.tokle@gmail.com>
2025-05-02 11:03:59 +0530

    Updated Commit

--- app.js
+++ app.js
@@ -27,3 +27,56 @@ console.log(a);
 --a;
 console.log(a);
 
+
+console.log("Comparison Operators");
+
+console.log("Conditional Statement");
+
+let color="yellow";
+
+if(color=="red"){
+    console.log(`The color is ${color} and you have to stop`);
+}
+else if(color=="green"){
+    console.log(`the color is ${color} and you  can go`);
+}
+else{
+    console.log(`The color is ${color} and you have to drive slowly`);
+}
+
+//Good String
+
+console.log("Good String");
+
+let str="amit Suresh Chandure";
+
+if(str[0]=='a'&& str.length>3){
+    console.log(`The string ${str} is a good String`);
+}
+else{
+    console.log(`The string ${str} is not a good String`);
+
+}
+
+//Switch Variable
+
+let day=4;
+switch(day){
+    case 1:console.log("Monday");
+    break;
+    case 2:console.log("Tuesday");
+    break;
+    case 3:console.log("Wednesday");
+    break;
+    case 4:console.log("Thursday");
+    break;
+    case 5:console.log("Friday");
+    break;
+    case 6:console.log("Saturday");
+    break;
+    case 7:console.log("Sunday");
+    break;
+    default:console.log("Invalid Day");
+    break
+
+}
